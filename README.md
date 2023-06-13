f796759abe9b68030628336fffd3eb46cdd84442
Jesse Eisses <jesse.eisses@gmail.com>
2023-06-13 23:42:17 +0200

    Copy images from LabelBox to IPFS

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Effect Network LabelBox Scripts
 
+This script can also download images from the LabelBox instance and
+upload them to a private IPFS instance, to ensure the images stay
+accessible and behind HTTPS. For this to work, you must set your
+LabelBox session ID which can be fetched from the HTTP requests in an
+authenticated LabelBox instance:
+
+```
+export LB_SESSION_ID=".eJ....."
+```
+
 ### Examples
 
 ```
--- data.clj
+++ data.clj
@@ -1,22 +1,44 @@
 (ns data
   (:require [cheshire.core :as json]
             [clojure.data.csv :as csv]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [babashka.http-client :as http]))
 
 (def base-url "http://3.76.196.166:8080")
+(def auth-cookie (str "sessionid=" (System/getenv "LB_SESSION_ID")))
+(def ipfs-endpoint "https://ipfs.effect.ai/")
+
+(defn copy-file-to-ipfs [url]
+  (let [file (http/get
+              url
+              {:as :stream
+               :headers
+               {"cookie" auth-cookie}})
+        ipfs-hash
+        (->
+         (http/post (str ipfs-endpoint "api/v0/add")
+                    {:query-params {"pin" "true"}
+                     :multipart [{:name "file" :content (:body file)}]})
+         :body
+         json/decode
+         (get "Hash"))]
+    (str ipfs-endpoint "ipfs/" ipfs-hash)))
 
 (defn convert
   "Converts a full JSON export from LabelBox to a CSV for Effect
   Network. Image paths are constructed from the `base-url`."
-  [file-name]
+  [file-name copy-images?]
   (let [data (take 5 (json/decode (slurp file-name)))]
     (prn "Found " (count data) " data points")
     (let [task-data
           (concat
            [["id" "annotations" "image"]]
            (for [{:strs [id annotations data] :as data-point} data]
-             (let [img-url (str base-url (get data "ocr"))]
-               [id (json/encode annotations) img-url])))]
+             (let [img-url (str base-url (get data "ocr"))
+                   ipfs-img (if copy-images?
+                              (copy-file-to-ipfs img-url)
+                              img-url)]
+                  [id (json/encode annotations) ipfs-img])))]
 
       (with-open [writer (io/writer "task-data.csv")]
         (csv/write-csv writer task-data :quote \')))))
